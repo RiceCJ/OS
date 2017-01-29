4f2230fcc87cf2bf2d6a4ea7d52530e421a1b0c1
cj <ricezcj@gmail.com>
2017-01-29 16:33:55 -0600

    add basic functions, but not implementing them

--- montty.c
+++ montty.c
@@ -2,3 +2,62 @@
 // Created by Chengjiu Zhang on 1/29/17.
 //
 
+#include <threads.h>
+#include <hardware.h>
+#include <terminals.h>
+
+void TransmitInterrupt(int term){
+
+};
+
+void ReceiveInterrupt(int term){
+
+};
+
+void WriteDataRegister(int term, char c){
+
+};
+
+
+char ReadDataRegister(int term){
+
+};
+
+
+int InitHardware (int term){
+
+};
+
+int HardwareOutputSpeed(int term, int msecs){
+
+};
+
+
+int HardwareInputSpeed(int term, int msecs){
+
+};
+
+
+int WriteTerminal(int term, char *buf, int buflen){
+
+};
+
+
+int ReadTerminal(int term, char *buf, int buflen){
+
+};
+
+
+int InitTerminal(int term){
+
+};
+
+
+int TerminalDriverStatistics(struct termstat *stats){
+
+};
+
+
+int InitTerminalDriver(void){
+
+};
