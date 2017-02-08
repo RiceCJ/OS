a2e8d151c7dfa23f40873781d348f6b00316ff9b
cj <ricezcj@gmail.com>
2017-02-07 22:15:06 -0600

    test5

--- test5.c
+++ test5.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
     InitTerminalDriver();
     InitTerminal(1);
-//    InitTerminal(2);
+    InitTerminal(2);
     if (argc > 1) HardwareOutputSpeed(1, atoi(argv[1]));
     if (argc > 2) HardwareInputSpeed(1, atoi(argv[2]));
     if (argc > 1) HardwareOutputSpeed(2, atoi(argv[1]));
